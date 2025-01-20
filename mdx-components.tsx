760b47726fdc5853b2e1ac9a29f9d66bb03dd600
Dinesh M <dineshmuthuswami@gmail.com>
2025-01-21 00:22:22 +0530

    initial setup

---
+++ mdx-components.tsx
@@ -0,0 +1,10 @@
+import React, { ComponentPropsWithoutRef } from 'react';
+import { Link } from 'next-view-transitions';
+import { highlight } from 'sugar-high';
+
+type HeadingProps = ComponentPropsWithoutRef<'h1'>;
+type ParagraphProps = ComponentPropsWithoutRef<'p'>;
+type ListProps = ComponentPropsWithoutRef<'ul'>;
+type ListItemProps = ComponentPropsWithoutRef<'li'>;
+type AnchorProps = ComponentPropsWithoutRef<'a'>;
+type BlockquoteProps = ComponentPropsWithoutRef<'blockquote'>;
